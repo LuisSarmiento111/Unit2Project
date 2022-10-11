ac6d8d9c24be9062828be7969e919a56c0bfcd45
BT_4N2_17 <lsarmiento9311@bths.edu>
2022-10-11 12:31:00 -0400

    Have Design Document and created LinearEquation class. Create variables x1, x2, y1, y2, m, d, b, and equation.

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -3,4 +3,13 @@ public class LinearEquation {
     private int x2;
     private int y1;
     private int y2;
+    private double m;
+    private double b;
+    private double d;
+    private String equation;
+
+    public LinearEquation(int x1, int y1, int x2, int y2){
+        this.x1 = x1;
+        this.y1 = y1;
+    }
 }
