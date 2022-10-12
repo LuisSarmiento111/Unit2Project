53c359d1dc3a108998f79738b96e94e1f3043d2d
BT_4N2_17 <lsarmiento9311@bths.edu>
2022-10-12 12:09:36 -0400

    Created method calculateSlope to find & return slope Created method calculateDistance to find & return distance Created method calculateIntersect to find & return y-intersect Created method slopeIntersectForm to use variables to create an equation for the points. Created toString method to print out following information about the points. Created EquationRunner.java to run and test code in LinearEquation.java

---
+++ src/EquationRunner.java
@@ -0,0 +1,7 @@
+public class EquationRunner {
+    public static void main(String[] args){
+         LinearEquation taco = new LinearEquation(-4,2,-5,3);
+         System.out.println(taco.toString());
+
+    }
+}
--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -3,13 +3,49 @@ public class LinearEquation {
     private int x2;
     private int y1;
     private int y2;
-    private double m;
-    private double b;
-    private double d;
+    private int m;
+    private int b;
+    private double distance;
     private String equation;
 
     public LinearEquation(int x1, int y1, int x2, int y2){
         this.x1 = x1;
         this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
     }
+
+    public int calculateSlope(){
+        m = (y2 - y1) / (x2 -x1);
+        return m;
+    }
+
+    public double calculateDistance(){
+        distance = Math.sqrt(Math.pow(x2 - x1,2) + Math.pow(y2-y1,2));
+        return Math.round(distance * 100.0) / 100.0;
+    }
+
+    public int calculateIntersect(){
+        if (x1 < 0)
+                b = y1 + (Math.abs(x1) * m);
+        else if (x1 > 0)
+            b = y1 - (x1 * m);
+        else
+            b = x1;
+        return b;
+    }
+
+    public String slopeIntersectForm(){   // Make it print the equation as a fraction and not a decimal and for negative y-intersect
+        equation = m + ("x + ") + (double) b;
+        return equation;
+    }
+
+    public String toString(){
+        String pair1 = "(" + x1 + (",") + y1 + (")");
+        String pair2 = "(" + x2 + (",") + y2 + (")");
+        String s = "First Pair: " + pair1 + ("\nSecond Pair: " + pair2 + ("\nSlope of Line: ") + calculateSlope() + ("\nY-intersect: ") + calculateIntersect() + ("\nSlope intersect form: ") + slopeIntersectForm());
+        return s;
+    }
+
+
 }
