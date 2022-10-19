8f0eb7bea20634bf283f2380bc0d00b8513ec3d9
lasar <lsarmiento9311@bths.edu>
2022-10-18 21:39:26 -0400

    Created a way for it to take the user's input and assign it to other values to then make a LinearEquation object.

--- src/EquationRunner.java
+++ src/EquationRunner.java
@@ -1,8 +1,18 @@
 import java.util.Scanner;
 public class EquationRunner {
     public static void main(String[] args){
+        System.out.print("Enter the first x-y coordinate point: ");
         Scanner myObj = new Scanner(System.in);
-        LinearEquation taco = new LinearEquation(-4,2,-5,3);
+        String firstCoordinate = myObj.nextLine();
+        int comma = firstCoordinate.indexOf(",");
+        int x1 = Integer.valueOf(firstCoordinate.substring(1,comma));
+        int y1 = Integer.valueOf(firstCoordinate.substring(comma +1,firstCoordinate.length()-1));
+        System.out.print("Enter the second x-y coordinate point: ");
+        String secondCoordinate = myObj.nextLine();
+        int comma2 = secondCoordinate.indexOf(",");
+        int x2 = Integer.valueOf(secondCoordinate.substring(1,comma2));
+        int y2 = Integer.valueOf(secondCoordinate.substring(comma2 +1,firstCoordinate.length()-1));
+        LinearEquation taco = new LinearEquation(x1,y1,x2,y2);
         System.out.println(taco.toString());
         System.out.println("Enter an x-value: ");
 
