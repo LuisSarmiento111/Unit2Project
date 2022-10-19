08b1bfe662f2373049303cfa956e171f3102971d
lasar <lsarmiento9311@bths.edu>
2022-10-19 00:00:04 -0400

    Formatted my code

--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -8,30 +8,33 @@ public class LinearEquation {
     private double distance;
     private String equation;
 
-    public LinearEquation(int x1, int y1, int x2, int y2){
+    public LinearEquation(int x1, int y1, int x2, int y2) {
         this.x1 = x1;
         this.y1 = y1;
         this.x2 = x2;
         this.y2 = y2;
     }
 
-    public double calculateSlope(){   // make it double but print as int
+    public double calculateSlope() {   // make it double but print as int
         m = Math.round((y2 - y1) / (x2 -x1) * 100.0) / 100.0;
         return m;
     }
 
-    public double calculateDistance(){
+    public double calculateDistance() {
         distance = Math.sqrt(Math.pow(x2 - x1,2) + Math.pow(y2-y1,2));
         return Math.round(distance * 100.0) / 100.0;
     }
 
-    public double calculateIntersect(){
-        if (x1 < 0)
-                b = y1 + (Math.abs(x1) * m);
-        else if (x1 > 0)
+    public double calculateIntersect() {
+        if (x1 < 0) {
+            b = y1 + (Math.abs(x1) * m);
+        }
+        else if (x1 > 0) {
             b = y1 - (x1 * m);
-        else
+        }
+        else {
             b = x1;
+        }
         return Math.round(b * 100.0) / 100.0;
     }
 
@@ -45,7 +48,7 @@ public class LinearEquation {
         return gcd;
     }
 
-    public String decimalToFraction(){
+    public String decimalToFraction() {
         String slopeAsString = Double.toString(m * 100);
         int period = slopeAsString.indexOf(".");
         int numerator = Integer.valueOf(slopeAsString.substring(0,period));
@@ -55,17 +58,17 @@ public class LinearEquation {
         denominator = denominator / gcd;
         return numerator + "/" + denominator;
     }
-    public String slopeIntersectForm(){
+    public String slopeIntersectForm() {
         if (b < 0) {
             equation = decimalToFraction() + ("x - ") + Math.abs((double) b); // if b is negative, will change the "+" to a "-"
         }
-        else if (b >= 0){
+        else if (b >= 0) {
             equation = decimalToFraction() + ("x + ") + (double) b; // if b is not negative, will use the normal y = mx + b format
         }
         return equation;
     }
 
-    public String toString(){
+    public String toString() {
         String pair1 = "(" + x1 + (",") + y1 + (")");
         String pair2 = "(" + x2 + (",") + y2 + (")");
         String s = "First Pair: " + pair1 + ("\nSecond Pair: " + pair2 + ("\nSlope of Line: ") + calculateSlope()
@@ -74,7 +77,7 @@ public class LinearEquation {
         return s;
     }
 
-    public String calculateY(double x){ // Given an x-value, it will return the coordinate of that value according to the linear equation.
+    public String calculateY(double x) { // Given an x-value, it will return the coordinate of that value according to the linear equation.
         return "Solved coordinate point is: (" + x + ", " + (m * x + b) + ")";
     }
 }
