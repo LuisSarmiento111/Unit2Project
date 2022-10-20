3d72c6bb1017b8f04fb66a95d246efd00a616e32
lasar <lsarmiento9311@bths.edu>
2022-10-19 22:34:56 -0400

    Added comments to provide details on how specific pieces of code works and their purpose for being there. Fixed calculateIntersect method to correctly provide the y-intercept if x1 is equal to 0. Fixed calculateSlope to return slope values that are not whole if needed. Changed variable in toString to be more appropriate for the piece of code.

--- src/EquationRunner.java
+++ src/EquationRunner.java
@@ -1,21 +1,21 @@
 import java.util.Scanner;
 public class EquationRunner {
     public static void main(String[] args){
+        Scanner myObj = new Scanner(System.in); // scanner is created for user input
         System.out.print("Enter the first x-y coordinate point: ");
-        Scanner myObj = new Scanner(System.in);
-        String firstCoordinate = myObj.nextLine();
-        int comma = firstCoordinate.indexOf(",");
-        int x1 = Integer.valueOf(firstCoordinate.substring(1,comma));
-        int y1 = Integer.valueOf(firstCoordinate.substring(comma +1,firstCoordinate.length()-1));
+        String firstCoordinate = myObj.nextLine(); // assigns firstCoordinate given value
+        int comma = firstCoordinate.indexOf(","); // finds index of the first coordinate comma and assigns it to comma variable
+        int x1 = Integer.valueOf(firstCoordinate.substring(1,comma)); // separates x-value from coordinate
+        int y1 = Integer.valueOf(firstCoordinate.substring(comma +1,firstCoordinate.length()-1)); // separates y-value from coordinate
         System.out.print("Enter the second x-y coordinate point: ");
-        String secondCoordinate = myObj.nextLine();
-        int comma2 = secondCoordinate.indexOf(",");
-        int x2 = Integer.valueOf(secondCoordinate.substring(1,comma2));
-        int y2 = Integer.valueOf(secondCoordinate.substring(comma2 +1,secondCoordinate.length()-1));
-        LinearEquation myEquation = new LinearEquation(x1,y1,x2,y2);
-        System.out.println(myEquation.toString());
-        System.out.print("Enter an x-value: ");
-        System.out.println(myEquation.calculateY(myObj.nextDouble()));
+        String secondCoordinate = myObj.nextLine(); // assigns secondCoordinate given value
+        int comma2 = secondCoordinate.indexOf(","); // finds index of the second coordinate comma and assigns it to comma2 variable
+        int x2 = Integer.valueOf(secondCoordinate.substring(1,comma2)); // separates x-value from coordinate
+        int y2 = Integer.valueOf(secondCoordinate.substring(comma2 +1,secondCoordinate.length()-1)); // separates y-value from coordinate
+        LinearEquation myEquation = new LinearEquation(x1,y1,x2,y2); // creates object myEquation with the four integer values as inputs
+        System.out.println(myEquation.toString()); // prints the information of the linear equation
+        System.out.print("Enter an x-value: "); // asks user to put in an x-value
+        System.out.println(myEquation.calculateY(myObj.nextDouble())); // prints the coordinate of that x-value
 
 
     }
--- src/LinearEquation.java
+++ src/LinearEquation.java
@@ -1,83 +1,83 @@
 public class LinearEquation {
-    private int x1;
-    private int x2;
-    private int y1;
-    private int y2;
-    private double m;
-    private double b;
-    private double distance;
-    private String equation;
+    private int x1; // represents x-value of first coordinate
+    private int x2; // represents x-value of second coordinate
+    private int y1; // represents y-value of first coordinate
+    private int y2; // represents y-value of second coordinate
+    private double m; // represents slope
+    private double b; // represents y-intercept
+    private double distance; // represents the distance between the two points
+    private String equation; // represents the equation of line
 
-    public LinearEquation(int x1, int y1, int x2, int y2) {
+    public LinearEquation(int x1, int y1, int x2, int y2) { // creates an object using four int parameters and assigns the x and y-values accordingly
         this.x1 = x1;
         this.y1 = y1;
         this.x2 = x2;
         this.y2 = y2;
     }
 
-    public double calculateSlope() {   // make it double but print as int
-        m = Math.round((y2 - y1) / (x2 -x1) * 100.0) / 100.0;
+    public double calculateSlope() {  // method for calculating slope and returns it to the hundredths place
+        m = Math.round((double)(y2 - y1) / (x2 -x1) * 100.0) / 100.0;
         return m;
     }
 
-    public double calculateDistance() {
+    public double calculateDistance() { // method for calculating the distance using the formula and returns it to hundredths place
         distance = Math.sqrt(Math.pow(x2 - x1,2) + Math.pow(y2-y1,2));
         return Math.round(distance * 100.0) / 100.0;
     }
 
-    public double calculateIntersect() {
-        if (x1 < 0) {
+    public double calculateIntersect() { // method for calculating the y-intersect using the slope and returns it
+        if (x1 < 0) { // if the x-value of the first coordinate is positive, will calculate and assign b to a positive value
             b = y1 + (Math.abs(x1) * m);
         }
-        else if (x1 > 0) {
+        else if (x1 > 0) { // if the x-value of the first coordinate is negative, will calculate and assign b to a positive value
             b = y1 - (x1 * m);
         }
         else {
-            b = x1;
+            b = y1; // if the x-value is neither positive nor negative, essentially 0, b is assigned the first coordinate's y-value
         }
-        return Math.round(b * 100.0) / 100.0;
+        return Math.round(b * 100.0) / 100.0; // returns the y-intersect rounded to hundredths place
     }
 
-    public int findGCD(int n1, int n2) {
+    public int findGCD(int n1, int n2) { // method for finding the gcf of a fraction for simplifying, has two int parameters
         int gcd = 1;
-        for (int i = 1; i <= n1 && i <= n2; i++) {
-            if (n1 % i == 0 && n2 % i == 0) {
-                gcd = i;
+        for (int i = 1; i <= n1 && i <= n2; i++) { // assigned i to 1 and assumes it's less than n1 and n2. If condition not met, increase i by 1.
+            if (n1 % i == 0 && n2 % i == 0) { // if both the numerator and denominator can be fully divided by i, will set gcd to i's value.
+                    gcd = i;
             }
         }
         return gcd;
     }
 
-    public String decimalToFraction() {
-        String slopeAsString = Double.toString(m * 100);
-        int period = slopeAsString.indexOf(".");
-        int numerator = Integer.valueOf(slopeAsString.substring(0,period));
-        int denominator = 100;
-        int gcd = findGCD(Math.abs(numerator),denominator);
-        numerator = numerator / gcd;
-        denominator = denominator / gcd;
-        return numerator + "/" + denominator;
+    public String decimalToFraction() { // method for converting a decimal, assuming it's in hundredths place, to a fraction string
+        String slopeAsString = Double.toString(m * 100); // converts slope into a decimal with no values after decimal point
+        int period = slopeAsString.indexOf("."); // finds index of "." in the slope
+        int numerator = Integer.valueOf(slopeAsString.substring(0,period)); // assigns numerator to value before the decimal point
+        int denominator = 100; // assigns denominator to 100 (since this project has all slopes rounded to hundredths place).
+        int gcd = findGCD(Math.abs(numerator),denominator); // uses GCD method to assign gcd a value
+        numerator = numerator / gcd; // simplifies numerator
+        denominator = denominator / gcd; // simplifies denominator
+        return numerator + "/" + denominator; // returns the divided values in string form to represent a fraction
     }
-    public String slopeIntersectForm() {
-        if (b < 0) {
+    public String slopeIntersectForm() { // method for returning the equation of the line in slope-intersect form
+        if (b < 0) {  // if y-intersect is negative, will have equation have a "-" for y-intercept
             equation = decimalToFraction() + ("x - ") + Math.abs((double) b); // if b is negative, will change the "+" to a "-"
         }
-        else if (b >= 0) {
+        else if (b >= 0) { // if y-intersect is 0 or positive, will have equation have a "+" for y-intercept
             equation = decimalToFraction() + ("x + ") + (double) b; // if b is not negative, will use the normal y = mx + b format
         }
         return equation;
     }
 
-    public String toString() {
-        String pair1 = "(" + x1 + (",") + y1 + (")");
-        String pair2 = "(" + x2 + (",") + y2 + (")");
-        String s = "First Pair: " + pair1 + ("\nSecond Pair: " + pair2 + ("\nSlope of Line: ") + calculateSlope()
+    public String toString() { // method for returning all the information needed about the linear equation
+        String pair1 = "(" + x1 + (",") + y1 + (")"); // combines x1 and y1 to make first coordinate
+        String pair2 = "(" + x2 + (",") + y2 + (")"); // combines x2 and y2 to make second coordinate
+        String information = "First Pair: " + pair1 + ("\nSecond Pair: " + pair2 + ("\nSlope of Line: ") + calculateSlope()
                 + ("\nY-intersect: ") + calculateIntersect() + ("\nSlope intersect form: ") + slopeIntersectForm()
-                + ("\nDistance between points: ") + calculateDistance());
-        return s;
+                + ("\nDistance between points: ") + calculateDistance()); // puts all information into a string
+        return information;
     }
 
     public String calculateY(double x) { // Given an x-value, it will return the coordinate of that value according to the linear equation.
-        return "Solved coordinate point is: (" + x + ", " + (m * x + b) + ")";
+        return "Solved coordinate point is: (" + x + ", " + (m * x + b) + ")"; // uses mx + b to find y and puts it into coordinate
     }
 }
